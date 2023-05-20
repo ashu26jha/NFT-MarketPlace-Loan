cad492d99f29bc95517e19bfc28055541700d0ed
Ashutosh Jha <ashutosh26jha@gmail.com>
2023-05-20 16:56:51 +0530

    NFT contract

---
+++ Hardhat Backend/contracts/KittyNft.sol
@@ -0,0 +1,59 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+// Robot/AI NFT implementation 
+
+// OpenZeppelin Files
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+// Error reverts
+error RobotNft__NotEnoughETH();
+error RobotNft__DoesNotExist();
+error RobotNft__AlreadyMinted();
+
+contract RobotNft is ERC721{
+    
+    // Immutable variables
+    uint256 private immutable i_mintFee;
+    
+    // List of token URIs
+    string [] private tokenURIs = [
+        "bafkreiaidflzvyhguszit4kiid3lilzwihdpfx7jgad6enofaphyn7jdaa",
+        "bafkreigi2ylq474d5qnw3txs6bmvpra3mg33lwrbft2ti73csb3xqbtivm",
+        "bafkreibmycdy5eklvwtwbls4kzwdhpul2xzivldtohptffj5owxwego62m",
+        "bafkreig3sifqmfin4omzo3weujrax6icqux3kftazpqhuss25dg5tbwjwi",
+        "bafkreigvnflgpovgia7fgwotb5rx7lvlp4mekwqkrzzqsw2tigx4fjgije",
+        "bafkreibui3vkbdn5r7ocyczru6nhnznljqcrhm7rc2nbh4ahpvtxpphisu",
+        "bafkreigobgcjxo7oheetgsdqm54ujpscuurow363ik2ope25rzjalfpwum",
+        "bafkreiepmk3wl5ns3266h2qcsvtohkl572hmm3i2khvvyja54orjddbqxu"
+    ];
+    
+    // Mapping of minted NFTs
+    // TokenID => Boolean (Minted or not)
+    mapping(uint256 => bool) minted;
+
+    constructor(uint256 _mintFee) ERC721("Robot","RBT"){
+        i_mintFee = _mintFee;
+    }
+
+    function mintNFT(uint256 tokenId) payable external{
+        if (msg.value < i_mintFee){
+            revert RobotNft__NotEnoughETH();
+        }
+        if (tokenId > 7){
+            revert RobotNft__DoesNotExist();
+        }
+        if(minted[tokenId]==true){
+            revert RobotNft__AlreadyMinted();
+        }
+
+        _safeMint(msg.sender,tokenId);
+        minted[tokenId] = true;
+    }
+
+    // MintStatus
+    function mintStatus(uint256 tokenId) public view returns(bool){
+        return minted[tokenId];
+    }
+
+}
---
+++ Hardhat Backend/contracts/RobotNft.sol
@@ -0,0 +1,59 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+// Robot/AI NFT implementation 
+
+// OpenZeppelin Files
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+// Error reverts
+error RobotNft__NotEnoughETH();
+error RobotNft__DoesNotExist();
+error RobotNft__AlreadyMinted();
+
+contract RobotNft is ERC721{
+    
+    // Immutable variables
+    uint256 private immutable i_mintFee;
+    
+    // List of token URIs
+    string [] private tokenURIs = [
+        "bafkreid4u3zcjdnnognsqfq2n5xzbub4ear4pxvadpjyvcfamkyn7xnh5y",
+        "bafkreibho65hloym3fwcvdgluzgnxznhqb6kjbxd444oocuk7dtalf2p5a",
+        "bafkreihrq3flvy2gh6rzxzn43gxmtvir2iuhqgszfhtg2g2top2iid543i",
+        "bafkreihxnuuu4ueme77isilcwaqy7fndzk6kyxpo2khxma4ni6fkjvh7va",
+        "bafkreihdhj7u4ecyq7dchp7fl2b7fcp4ggddlcuhqnt7y35fcpj4zt5dqm",
+        "bafkreihirsrq2obrvpggor33ifm4enw6msnzspiokm7d7gndwgmuo6lqiq",
+        "bafkreiget4ew6h4ligxw7xn6undimoj5t6vq3nuo34smmznrekitxa7rte",
+        "bafkreigd6oulrbbdwyfn7wrjusfrid4dktbqd3cpgngmi5eopkbxvwhgnu"
+    ];
+    
+    // Mapping of minted NFTs
+    // TokenID => Boolean (Minted or not)
+    mapping(uint256 => bool) minted;
+
+    constructor(uint256 _mintFee) ERC721("Robot","RBT"){
+        i_mintFee = _mintFee;
+    }
+
+    function mintNFT(uint256 tokenId) payable external{
+        if (msg.value < i_mintFee){
+            revert RobotNft__NotEnoughETH();
+        }
+        if (tokenId > 7){
+            revert RobotNft__DoesNotExist();
+        }
+        if(minted[tokenId]==true){
+            revert RobotNft__AlreadyMinted();
+        }
+
+        _safeMint(msg.sender,tokenId);
+        minted[tokenId] = true;
+    }
+
+    // MintStatus
+    function mintStatus(uint256 tokenId) public view returns(bool){
+        return minted[tokenId];
+    }
+
+}
