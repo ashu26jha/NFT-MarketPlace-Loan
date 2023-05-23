7d83e53c0766320001f6d3bcff03cd68dffb901c
Ashutosh Jha <ashutosh26jha@gmail.com>
2023-05-24 01:07:41 +0530

    Need to add events everywhere... Contract overhaul needed

--- nextjs-frontend/pages/mintnft.js
+++ nextjs-frontend/pages/mintnft.js
@@ -1,13 +1,14 @@
 import Head from 'next/head';
 import Navbar from "../components/Navbar";
 import { useWeb3Contract } from "react-moralis";
-import {RobotNftAddress,CatNftAddress,chainID}from "./helper";
+import helper from "./helper";
 import RobotNftABI from '../constants/RobotNft.json';
 import CatNftABI from '../constants/CatNft.json';
 import Link from 'next/link';
 
 export default function mintnft(){
-
+    let RobotNftAddress, CatNftAddress, chainID;
+    [RobotNftAddress,CatNftAddress,chainID] = helper()
     return(
         <>
             <Navbar/>
